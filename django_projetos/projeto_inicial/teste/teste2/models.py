d0b9b264e50ba518e56b05f8b0e9b1b8bbc00f3b
AtlyGood <anthony236marques@gmail.com>
2025-06-17 17:28:47 -0300

    criando o banco de dados

--- django_projetos/projeto_inicial/teste/teste2/models.py
+++ django_projetos/projeto_inicial/teste/teste2/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class BancoModel(models.Model):
+    email = models.CharField(max_length=255)
+    senha = models.CharField(max_length=100)
+    nome = models.CharField(max_length=30)
+    sobrenome = models.CharField(max_length=20)
+    criacaodeconta = models.DateField(auto_now_add=True)
+    
+    
